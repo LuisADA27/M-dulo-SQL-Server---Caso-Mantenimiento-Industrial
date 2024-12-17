9ddda3aa51a94a7f1e2c4fda8f02ea664bf0ffed
luis <luisd275@gmail.com>
2024-12-17 01:11:34 -0500

    insert data mantto

--- Insert_data/insert data.sql
+++ Insert_data/insert data.sql
@@ -54,3 +54,19 @@ VALUES
 ('47', '2024-02-01 00:00:00', '2027-02-01 00:00:00', 'soporte en auditorias', 'activo', '2024-01-15 10:00:00', '2024-12-10 20:00:00', '', '8', '1', ''),
 ('43', '2023-10-01 00:00:00', '2025-10-01 00:00:00', 'software gestion mantenimiento', 'activo', '2023-08-01 08:00:00', '2024-12-10 20:00:00', '', '9', '1', ''),
 ('41', '2024-03-01 00:00:00', '2027-03-01 00:00:00', 'software gestion mantenimiento', 'activo', '2024-02-01 08:00:00', '2024-12-10 20:00:00', '', '10', '1', '');
+
+--Insertar Mantenimientos
+SELECT*FROM Mantenimientos;
+
+INSERT INTO [dbo].[Mantenimientos] ([contrato_id], [equipos_id], [fecha_programada],[fecha_ejecutada], [tipo_mantenimiento], [comentarios])
+VALUES
+('4', '1', '2024-02-25 00:00:00', '2024-02-30 00:00:00', 'preventivo', 'los repuestos no llegaron a tiempo por lo que la calidad del servicio fue afectado'),
+('5', '5', '2023-02-01 00:00:00', '2024-01-01 00:00:00', 'correctivo', 'desgaste prematuro en generador'),
+('6', '3', '2024-12-01 00:00:00', '2024-12-01 00:00:00', 'predictivo', 'se observa alta vibracion en unidad motriz'),
+('7', '7', '2024-06-15 00:00:00', '2024-07-15 00:00:00', 'preventivo', 'se observa alta polucion en tarjetas electronicas'),
+('8', '2', '2023-08-01 00:00:00', '2023-08-01 00:00:00', 'preventivo', 'se observa alto consumo de amperaje en motor'),
+('9', '2', '2024-12-15 00:00:00', '2024-12-15 00:00:00', 'correctivo', 'se observa alta vibracion en motor'),
+('10', '7', '2025-04-01 00:00:00', '2024-12-15 00:00:00', 'correctivo', 'se observa falla de unidad de inversion de CA'),
+('11', '4', '2024-06-01 00:00:00', '2024-06-01 00:00:00', 'preventivo', 'se observa alta vibracion en unidad motriz'),
+('12', '6', '2024-07-01 00:00:00', '2024-07-01 00:00:00', 'preventivo', 'se observa alta contaminacion en equipo'),
+('13', '5', '2024-11-01 00:00:00', '2024-12-01 00:00:00', 'preventivo', 'se observa alta vibracion en unidad generadora');
